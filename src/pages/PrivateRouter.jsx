8461c84f0d4104b12c94951bb482007b96b28cbc
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-22 11:45:32 +0300

    finish

--- src/pages/PrivateRouter.jsx
+++ src/pages/PrivateRouter.jsx
@@ -5,7 +5,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const PrivateRouter = () => {
     const userAuthenticated = true
   return <div>
-    {userAuthenticated ? <Outlet/> : <Navigate to="Login" /> }</div>;
+    {userAuthenticated ? <Outlet/> : <Navigate to="/Login" /> }</div>;
 }
 
 export default PrivateRouter
